f6525a41aa05368a2f7558ff1019417ad8b6208e
RutC9 <85844964+RutC9@users.noreply.github.com>
2022-06-10 12:35:58 +0530

    Update connect_database.php

--- connect_database.php
+++ connect_database.php
@@ -4,9 +4,9 @@ This file contains database connection configurationuser "root" and password ""
 */
 
 define('DB_SERVER', 'localhost');/* localhost */ /* sql110.epizy.com */
-define('DB_USERNAME', 'root');/* root */ /* epiz_31269725 */
-define('DB_PASSWORD', '');/*  */ /* 2lg2R4N0nR6jgWW */
-define('DB_NAME', 'wp_project');/* wp_project */ /* epiz_31269725_pcbuilding_rut */
+define('DB_USERNAME', 'root');/* root */ 
+define('DB_PASSWORD', '');/*  */ 
+define('DB_NAME', 'wp_project');/* wp_project */ 
 
 // $database - variable
 $database = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
